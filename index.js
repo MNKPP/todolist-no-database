feb7b3dab5f46b7622cbac7167d7e3a2212a8368
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-01-03 22:09:59 +0800

    Add body parsing for the add task functionnality and use fs to add a task in a file.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug")
 
+app.use(express.urlencoded({extended: false}));
 app.use(mainRouter);
 
 app.listen(3000, () => {
--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const taskRouter = express.Router();
 
 taskRouter.post('/add-task', (req, res) => {
-    res.send('<h1>Add Task</h1>');
+    const body = req.body;
+    const jsonBodyParsed = JSON.stringify(body);
+    const jsonFilePath = path.join(path.resolve(__dirname, "../"), "public", "data.json");
+
+
+    if (!fs.existsSync(jsonFilePath)) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        fs.appendFileSync(jsonFilePath, jsonBodyParsed);
+        res.redirect('/');
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send(err);
+    }
+
 })
 
 module.exports = taskRouter;
