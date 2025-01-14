314eb52375a577739fd05e668ecc61b3a0719209
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-01-14 19:15:18 +0800

    Add task deletion functionality with frontend and backend support
    Implemented a delete button in the UI and a corresponding backend route to handle task deletions. The front end uses a DELETE request to remove tasks, which are filtered and saved back to the JSON file on the server. This improves task management by enabling users to delete tasks easily.

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,16 @@ const mainRouter = express.Router();
 
 const taskRouter = require('./task');
 
-// The most specific router first
 mainRouter.use(taskRouter);
 
 mainRouter.get('/', async (req, res) => {
-    // Read the task in a html list
     const jsonFilePath = path.join(path.resolve(__dirname, "../"), "public", "data.json");
 
     const fileContent = await fs.readFile(jsonFilePath);
 
     const fileContentParsed = JSON.parse(fileContent);
 
-    // Bien connaître l'objet pour itérer correctment. Procèder par étape
-    // Prochaine étape, savoir réaliser la boucle dans l'affichage des tâches
-    fileContentParsed.tasks.forEach(task => {
-        console.log(task.title)
-    })
-    res.render('index', {data: fileContentParsed.tasks});
+    res.render('index', {data: fileContentParsed});
 })
 
 
--- routes/task.js
+++ routes/task.js
@@ -4,34 +4,52 @@ const fs = require('fs').promises;
 
 const taskRouter = express.Router();
 
+const jsonFilePath = path.join(path.resolve(__dirname, "../"), "public", "data.json");
+
 taskRouter.post('/add-task', async (req, res) => {
     const body = req.body;
-    const jsonFilePath = path.join(path.resolve(__dirname, "../"), "public", "data.json");
 
     if (!body.task) {
-        // TODO : Possiblement le passer sur une route GET pour afficher les tâches
         res.redirect('/')
     }
 
     try {
         const fileContent = await fs.readFile(jsonFilePath);
-
         const fileContentParsed = JSON.parse(fileContent);
 
         const newData = {
-            id: fileContentParsed.tasks.length + 1,
+            id: fileContentParsed.length + 1,
             title: body.task
         }
 
-        fileContentParsed.tasks.push(newData)
+        fileContentParsed.push(newData)
 
         await fs.writeFile(jsonFilePath, JSON.stringify(fileContentParsed, null, 4));
         res.redirect('/');
     } catch (err) {
-        console.log(err)
         return res.status(400).send(err);
     }
 
 })
 
+taskRouter.delete('/delete-task/:id', async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const fileContent = await fs.readFile(jsonFilePath);
+        const fileContentParsed = JSON.parse(fileContent);
+
+        const filteredTasks = fileContentParsed.filter(item => item.id !== id);
+
+        await fs.writeFile(jsonFilePath, JSON.stringify(filteredTasks, null, 4));
+
+        return res.status(200).send(
+            {
+                message: 'Task deleted successfully'
+            }
+        )
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+})
 module.exports = taskRouter;
